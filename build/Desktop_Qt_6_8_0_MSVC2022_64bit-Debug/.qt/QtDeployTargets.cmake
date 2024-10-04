137d5a122fe82915db97e7c398b09412600f1b79
DanteS <danya.ladanov@gmail.com>
2024-10-04 21:55:43 +0300

    Start

---
+++ build/Desktop_Qt_6_8_0_MSVC2022_64bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_Python_programming_handbook_FILE D:/QtProjects/Python_programming_handbook/build/Desktop_Qt_6_8_0_MSVC2022_64bit-Debug/Python_programming_handbook.exe)
+set(__QT_DEPLOY_TARGET_Python_programming_handbook_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_Python_programming_handbook_RUNTIME_DLLS C:/Qt/6.8.0/msvc2022_64/bin/Qt6Widgetsd.dll;C:/Qt/6.8.0/msvc2022_64/bin/Qt6Guid.dll;C:/Qt/6.8.0/msvc2022_64/bin/Qt6Cored.dll)
